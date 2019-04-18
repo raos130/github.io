7440dfcf0f2f568d195e3b4f02cee301ad350786
raos130 <raos130@newschool.edu>
2019-04-17 21:58:49 -0400

    api

---
+++ projects/madlib/main.js
@@ -0,0 +1,24 @@
+
+$.get('https://raos130.github.io/api.json', function(data) {
+  console.log(data);
+ // var adjective1= data.adjective1;
+
+ $('.word-adjective1').html(data.adjective1);
+  $('.word-adjective2').html(data.adjective2);
+  $('.word-pluralNoun3').html(data.pluralNoun3);
+  $('.word-websiteName').html(data.websiteName);
+  $('.word-pluralNoun1').html(data.pluralNoun1);
+  $('.word-noun2').html(data.noun2);
+  $('.word-noun1').html(data.noun1);
+  $('.word-verbEndingInIng').html(data.verbEndingInIng);
+  $('.word-artSupply').html(data.artSupply);
+  $('.word-numbers').html(data.numbers);
+  $('.word-celebrityName').html(data.celebrityName);
+  $('.word-artistName').html(data.artistName);
+  $('.word-bodyPart').html(data.bodyPart);
+  $('.word-foods').html(data.foods.join(', '));
+
+
+}).fail(function() {
+  console.log('Error in GET request. Handle the error gracefully.');
+});
