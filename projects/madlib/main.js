af056cf57f53be02abb86e733c1410f5eee61b0d
raos130 <raos130@newschool.edu>
2019-04-17 22:31:49 -0400

    api

--- projects/madlib/main.js
+++ projects/madlib/main.js
@@ -3,7 +3,7 @@ $.get('https://raos130.github.io/api.json', function(data) {
   console.log(data);
  // var adjective1= data.adjective1;
 
- $('.word-adjective1').html(data.adjective1);
+  $('.word-adjective1').html(data.adjective1);
   $('.word-adjective2').html(data.adjective2);
   $('.word-pluralNoun3').html(data.pluralNoun3);
   $('.word-websiteName').html(data.websiteName);
@@ -15,8 +15,12 @@ $.get('https://raos130.github.io/api.json', function(data) {
   $('.word-numbers').html(data.numbers);
   $('.word-celebrityName').html(data.celebrityName);
   $('.word-artistName').html(data.artistName);
+  $('.word-pluralNoun2').html(data.pluralNoun2);
   $('.word-bodyPart').html(data.bodyPart);
+  $('.word-computerSoftware').html(data.computerSoftware);
   $('.word-foods').html(data.foods.join(', '));
+    $('.word-pluralNoun3').html(data.pluralNoun3);
+    $('.word-placeInNewYork').html(data.placeInNewYork);
 
 
 }).fail(function() {
